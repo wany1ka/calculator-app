85a9c9c5d47f3369824626a83efe8eb3e05951ff
wany1ka <markwanyika@gmail.com>
2024-04-08 19:29:32 +0300

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
 import React, { useState } from 'react'
- import './App.css'
+import './App.css'
 function App() {
   const [value, setValue] = useState('');
   return (
-   <div className="container">
-    <div className="calculator">
-      <form action="">
-        <div className="display">
-          <input type="text" value= {value} />
-        </div>
-        <div className='calculations'>
-          <input type="button" value="AC" onClick={e => setValue('')}/>
-          <input type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))}/>
-          <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
-          <input type="button" value="/" onClick={e => setValue(value + e.target.value)}/>
-        </div>
-        <div>
-        <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="*" onClick={e => setValue(value + e.target.value)}/>
-        </div>
-        <div>
-        <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="5" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="6" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="+" onClick={e => setValue(value + e.target.value)}/>
-        </div>
-        <div>
-        <input type="button" value="1" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="2" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="3" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
-        </div>
-        <div>
-        <input type="button" value="0" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="=" className="equal" onClick={e => setValue(eval(value))}/>
-        </div>
-      </form>
+    <div className="container bg-orange-900">
+      <div className="calculator">
+        <form action="">
+          <div className="display">
+            <input type="text" value={value} />
+          </div>
+          <div className='calculations'>
+            <input className='button bg-red-950' type="button" value="AC" onClick={e => setValue('')} />
+            <input className='button bg-red-950' type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))} />
+            <input className='button bg-red-700' type="button" value="." onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-red-700' type="button" value="/" onClick={e => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input className='button bg-amber-300' type="button" value="7" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="8" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="9" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-red-700' type="button" value="*" onClick={e => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input className='button bg-amber-300' type="button" value="4" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="5" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="6" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-red-700' type="button" value="+" onClick={e => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input className='button bg-amber-300' type="button" value="1" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="2" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-amber-300' type="button" value="3" onClick={e => setValue(value + e.target.value)} />
+            <input className='button bg-red-700' type="button" value="-" onClick={e => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input className='button bg-amber-300' type="button" value="0" onClick={e => setValue(value + e.target.value)} />
+            <input className='equal' type="button" value="=" onClick={e => setValue(eval(value))} />
+          </div>
+        </form>
+      </div>
     </div>
-   </div>
   )
 }
 export default App
